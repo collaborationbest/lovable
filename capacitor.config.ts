6646c77ba23a44e2150cab9ecb5ad7c7a9836cbf
collaborationbest <jericoaragon.senior@gmail.com>
2025-05-01 18:32:18 -0700

    feat: added capacitor.config file

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.0f9c9eedf5cb4db2b116d1be3bc5ee19',
+  appName: 'dentalpilote-4b097d7a',
+  webDir: 'dist',
+  server: {
+    url: 'https://0f9c9eed-f5cb-4db2-b116-d1be3bc5ee19.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  },
+};
+
+export default config;
